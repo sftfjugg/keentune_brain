2be7a570bb0a538decd00216679b9ff4c9958aef
runzhe.wrz <runzhe.wrz@alibaba-inc.com>
2021-10-26 11:25:51 +0800

    style: modify setup scripts, remove requirements of pbr
    Signed-off-by: runzhe.wrz <runzhe.wrz@alibaba-inc.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(setup_requires=['pbr'], pbr=True)
+long_description = ""
+
+setup(
+    name = "keentune-brain",
+    version = "1.0.0",
+    description = "KeenTune brain unit",
+    long_description = long_description,
+    url = "https://codeup.openanolis.cn/codeup/keentune/keentune_brain",
+    license = "Apache License",
+    classifiers = [
+        "Environment:: KeenTune",
+        "IntendedAudience :: Information Technology",
+        "IntendedAudience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "ProgrammingLanguage :: Python"
+    ],
+
+    packages = find_packages(),
+    package_data={'brain': ['brain.conf']},
+    
+    data_files = [
+        ("/etc/keentune/conf", ["brain/brain.conf"]),
+    ],
+)
