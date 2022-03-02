caf19eb8d28ccc2bcc2adfe8a2f719391b08d8e8
runzhe.wrz <15501019889@126.com>
2022-03-02 10:43:40 +0000

    !10 refactor tornado module: replace await by threadpool * Merge branch 'master' of gitee.com:anolis/keentune_brain into auto-100… * refactor tornado module: replace await by threadpool

--- brain/controller/sensi.py
+++ brain/controller/sensi.py
@@ -1,12 +1,71 @@
 import json
-from tornado.web import RequestHandler
+import traceback
 
-from brain.common.system import HTTPPost
 from brain.algorithm.sensitize.sensitize import sensitize
 from brain.common.config import AlgoConfig
 
+from tornado.web import RequestHandler
+from tornado.httpclient import HTTPClient, HTTPRequest, HTTPError
+from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
+from tornado.gen import coroutine
+
+
 class sensitizeHandler(RequestHandler):
-    async def post(self):
+    executor = ThreadPoolExecutor(20)
+
+    @run_on_executor
+    def _response(self,
+                  response_data:dict,
+                  response_ip  :str,
+                  response_port:str):
+        http_client = HTTPClient()
+        try:
+            response = http_client.fetch(HTTPRequest(
+                url    = "http://{ip}:{port}/sensitize_result".format(
+                            ip = response_ip, port = response_port),
+                method = "POST",
+                body   = json.dumps(response_data)
+            ))
+        except RuntimeError as e:
+            return False, "{},{}".format(e, traceback.format_exc())
+
+        except HTTPError as e:
+            return False, "{},{}".format(e, traceback.format_exc())
+
+        except Exception as e:
+            return False, "{},{}".format(e, traceback.format_exc())
+
+        else:
+            if response.code == 200:
+                return True, ""
+            else:
+                return False, response.reason
+
+        finally:
+            http_client.close()
+
+
+    @run_on_executor
+    def _sensitizeImpl(self, data_name, trials):
+        return sensitize(
+            data_name = data_name, 
+            trials    = trials, 
+            explainer = AlgoConfig.sensi_explainer, 
+            epoch     = AlgoConfig.sensi_epoch, 
+            topN      = AlgoConfig.sensi_topN,
+            threshold = AlgoConfig.sensi_threshold
+        )
+
+
+    @coroutine
+    def post(self):
+        def _validField(request_data):
+            assert request_data.__contains__('trials')
+            assert request_data.__contains__('resp_ip')
+            assert request_data.__contains__('resp_port')
+            assert request_data.__contains__('data')
+
         request_data = json.loads(self.request.body)
         # load explainer and epoch from config
         explainer = AlgoConfig.sensi_explainer
@@ -15,48 +74,24 @@ class sensitizeHandler(RequestHandler):
         threshold = AlgoConfig.sensi_threshold
 
         try:
-            trials    = int(request_data['trials'])
-            resp_ip   = request_data['resp_ip']
-            data_name = request_data['data']
-            resp_port = request_data['resp_port']
-
-        except KeyError as error_key:
-            self.write(json.dumps({
-                "suc": False,
-                "msg": "can not find key: {}".format(error_key)
-
-            }))
-            self.set_status(400)
+            _validField(request_data)
+        
+        except Exception as e:
+            self.write(json.dumps({"suc" : False, "msg": str(e)}))
             self.finish()
 
         else:
-            self.write(json.dumps({
-                "suc": True,
-                "msg": ""
-            }))
-            self.set_status(200)
+            self.write(json.dumps({"suc": True,"msg": ""}))
             self.finish()
 
-            try:
-                suc, res = sensitize(data_name, explainer, trials, epoch, topN, threshold)
-            
-            except Exception as e:
-                response_data = {"suc": False, "result": {}, "msg": "{}".format(e)}
-            
+            suc, out = yield self._sensitizeImpl(request_data['data'], int(request_data['trials']))
+            if suc:
+                response_data = {"suc": suc, "result": out, "msg": ""}
             else:
-                if suc:
-                    response_data = {
-                        "suc": True, "result": res, "msg": ""}
-                else:
-                    response_data = {
-                        "suc": False, "result": {}, "msg": res}
+                response_data = {"suc": suc, "result": {}, "msg": out}
 
-            await HTTPPost(
-                api="sensitize_result",
-                ip=resp_ip,
-                port=resp_port,
-                data=response_data
-            )
+            _, msg = yield self._response(response_data, request_data['resp_ip'], request_data['resp_port'])
+            print(msg)
 
 
 class sensiGraphHandler(RequestHandler):
