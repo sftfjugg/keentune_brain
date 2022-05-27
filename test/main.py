9e6de877687fdbe17b0e811372d9c4ce7ec5b77b
shaojing <986461335@qq.com>
2022-05-27 07:22:23 +0000

    !34 add module test cases related to brain service * test: add module test cases related to brain service

--- test/main.py
+++ test/main.py
@@ -2,56 +2,21 @@ import os
 import sys
 import unittest
 
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
+from test_brain_init import TestBrainInit
+from test_brain_acquire import TestBrainAcquire
 
-from MT_restful.test_target_configure import TestTargetConfigure
-from MT_restful.test_target_backup import TestTargetBackup
-from MT_restful.test_target_rollback import TestTargetRollback
-from MT_restful.test_target_status import TestTargetStatus
-from MT_restful.test_bench_sendfile import TestBenchSendfile
-from MT_restful.test_bench_benchmark import TestBenchBenchmark
-from MT_restful.test_bench_status import TestBenchStatus
-from MT_restful.test_brain_init import TestBrainInit
-from MT_restful.test_brain_acquire import TestBrainAcquire
-from MT_restful.test_brain_feedback import TestBrainFeedback
-from MT_restful.test_brain_best import TestBrainBest
-from MT_restful.test_brain_end import TestBrainEnd
-from MT_restful.test_brain_sensitize import TestBrainSensitize
-from MT_restful.test_keentuned_apply_result import TestKeentunedApplyResult
-from MT_restful.test_keentuned_benchmark_result import TestKeentunedBenchmarkResult
-from MT_restful.test_keentuned_sensitize_result import TestKeentunedSensitizeResult
-from MT_restful.test_brain_sensitize_list import TestKeentuneSensitizeList
-from MT_restful.test_brain_sensitize_delete import TestKeentuneSensitizeDelete
+os.chdir(os.path.abspath(os.path.join(os.getcwd(), "test")))
 
 
 def RunModelCase():
     suite = unittest.TestSuite()
-
-    suite.addTest(TestTargetConfigure('test_target_server_FUN_configure'))
-    suite.addTest(TestTargetBackup('test_target_server_FUN_backup'))
-    suite.addTest(TestTargetRollback('test_target_server_FUN_rollback'))
-    suite.addTest(TestTargetStatus('test_target_server_FUN_status'))
-
-    suite.addTest(TestBenchSendfile('test_bench_server_FUN_sendfile'))
-    suite.addTest(TestBenchBenchmark('test_bench_server_FUN_benchmark'))
-    suite.addTest(TestBenchStatus('test_bench_server_FUN_status'))
-
     suite.addTest(TestBrainInit('test_brain_server_FUN_init'))
     suite.addTest(TestBrainAcquire('test_brain_server_FUN_acquire'))
-    suite.addTest(TestBrainFeedback('test_brain_server_FUN_feedback'))
-    suite.addTest(TestBrainBest('test_brain_server_FUN_best'))
-    suite.addTest(TestBrainEnd('test_brain_server_FUN_end'))
-    suite.addTest(TestBrainSensitize('test_brain_server_FUN_sensitize'))
-
-    suite.addTest(TestKeentunedApplyResult('test_keentuned_server_FUN_apply_result'))
-    suite.addTest(TestKeentunedBenchmarkResult('test_keentuned_server_FUN_benchmark_result'))
-    suite.addTest(TestKeentunedSensitizeResult('test_keentuned_server_FUN_sensitize_result'))
-    suite.addTest(TestKeentuneSensitizeList('test_brain_server_FUN_sensitize_list'))
-    suite.addTest(TestKeentuneSensitizeDelete('test_brain_server_FUN_sensitize_delete'))
-
     return suite
 
 
 if __name__ == '__main__':
+    print("--------------- start to run test cases ---------------")
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(RunModelCase())
+    print("--------------- run test cases end ---------------")
---
+++ test/test_brain_acquire.py
@@ -0,0 +1,71 @@
+import unittest
+import requests
+import json
+from common import  brain_ip, brain_port
+
+class TestBrainAcquire(unittest.TestCase):
+    def setUp(self) -> None:
+        self.proxies={"http": None, "https": None}
+        url = "http://{}:{}/sensitize_list".format(brain_ip, brain_port)
+        re = requests.get(url, proxies=self.proxies)
+        if re.status_code != 200:
+            print("ERROR: Can't reach KeenTune-Brain.")
+            exit()
+            
+        url = "http://{}:{}/{}".format(brain_ip, brain_port, "init")
+        data = {
+            "algorithm":"tpe",
+            "iteration":500,
+            "name":"test",
+            "type":"tunning",
+            "parameters":[
+                {
+                    "name":"kernel.sched_migration_cost_ns",
+                    "range": [100000, 5000000],
+                    "dtype": "int",
+                    "step": 1,
+                    "domain": "sysctl",
+                    "base": 100000
+                    },
+                {
+                    "name": "kernel.randomize_va_space",
+                    "options": ["0", "1"],
+                    "dtype": "string",
+                    "domain": "sysctl",
+                    "base": "0"
+                    },
+                {
+                    "name": "kernel.ipv4.udp_mem",
+                    "options": ["16000 512000000 256 16000", "32000 1024000000 500 32000", "64000 2048000000 1000 64000"],
+                    "dtype": "string",
+                    "domain": "sysctl",
+                    "base": "16000 512000000 256 16000"
+                    }
+            ],
+            "baseline_score":{
+                "Requests_sec":{
+                    "base": [23047.33, 23754.47],
+                    "negative": False,
+                    "weight": 100,
+                    "strict": False
+                }
+            }
+        }
+
+        headers = {"Content-Type": "application/json"}
+        
+        result = requests.post(url, data=json.dumps(data), headers=headers, proxies=self.proxies)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.text, '{"suc": true, "msg": ""}')
+
+    def tearDown(self) -> None:
+        url = "http://{}:{}/{}".format(brain_ip, brain_port, "end")
+        result = requests.get(url, proxies=self.proxies)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.text, '{"suc": true, "msg": ""}')
+
+    def test_brain_server_FUN_acquire(self):
+        url = "http://{}:{}/{}".format(brain_ip, brain_port, "acquire")
+        result = requests.get(url, proxies=self.proxies)
+        self.assertEqual(result.status_code, 200)
+        self.assertIsNotNone(result.text)
