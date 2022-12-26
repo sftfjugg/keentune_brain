658dae05f0322735fa2f2372495728a698add557
shaojing <986461335@qq.com>
2022-12-26 06:26:25 +0000

    !79 add the available interface use case * test: add the available interface use case

--- test/main.py
+++ test/main.py
@@ -7,6 +7,7 @@ from test_brain_acquire import TestBrainAcquire
 from test_brain_feedback import TestBrainFeedback
 from test_brain_best import TestBrainBest
 from test_brain_end import TestBrainEnd
+from test_brain_available import TestBrainAvailable
 from test_brain_sensitize import TestBrainSensitize
 from test_brain_sensitize_list import TestBrainSensitizeList
 from test_brain_sensitize_delete import TestKeentuneSensitizeDelete
@@ -19,6 +20,7 @@ def RunModelCase():
     suite.addTest(TestBrainFeedback('test_brain_server_FUN_feedback'))
     suite.addTest(TestBrainBest('test_brain_server_FUN_best'))
     suite.addTest(TestBrainEnd('test_brain_server_FUN_end'))
+    suite.addTest(TestBrainAvailable('test_brain_server_FUN_available'))
     suite.addTest(TestBrainSensitize('test_brain_server_FUN_sensitize'))
     suite.addTest(TestBrainSensitizeList('test_brain_server_FUN_sensitize_list'))
     suite.addTest(TestKeentuneSensitizeDelete('test_brain_server_FUN_sensitize_delete'))
