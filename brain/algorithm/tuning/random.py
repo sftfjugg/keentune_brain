3493a9128f615de7ca8aa3766e0bc5b2956cf08f
liutian <2632789031@qq.com>
2023-03-17 14:01:39 +0800

    change tunning to tuning

---
+++ brain/algorithm/tuning/random.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from brain.algorithm.tuning.base import OptimizerUnit
+from brain.common import pylog
+
+
+class Random(OptimizerUnit):
+    @pylog.functionLog
+    def __init__(self,
+                 opt_name: str,
+                 max_iteration: int,
+                 knobs: list,
+                 baseline: dict,
+                 rule_list=None):
+        super(Random, self).__init__(opt_name, max_iteration, knobs, baseline, rule_list)
+
+    @pylog.functionLog
+    def acquireImpl(self):
+        config = {}
+        for param in self.knobs:
+            if param.__contains__('range'):
+                config[param['name']] = np.random.randint(
+                    param['range'][0], param['range'][1])
+
+            elif param.__contains__('options'):
+                config[param['name']] = param['options'][np.random.randint(
+                    0, param['options'].__len__())]
+
+            elif param.__contains__('sequence'):
+                config[param['name']] = param['sequence'][np.random.randint(
+                    0, param['sequence'].__len__())]
+
+            else:
+                raise Exception("unsupported parameter type!")
+                
+        return config, 1.0
+
+    @pylog.functionLog
+    def feedbackImpl(self, iteration: int, loss: float):
+        pass
+
+    @pylog.functionLog
+    def msg(self):
+        return "Random"
